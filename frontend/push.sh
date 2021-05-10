df3b43467cd5f30eefa049d00465e7978aef12cd
iamdempa <banukajananathjayarathna@gmail.com>
2021-05-10 15:32:23 +0530

    added push

---
+++ frontend/push.sh
@@ -0,0 +1,6 @@
+docker kill $(docker ps -q) || true && docker rm $(docker ps -a -q) || true && docker rmi -f $(docker images -q) 
+|| true
+
+docker build -t frontend .
+docker tag frontend 19950818/k8s:frontend 
+docker push 19950818/k8s:frontend
---
+++ mern/backend/push.sh
@@ -0,0 +1,6 @@
+docker kill $(docker ps -q) || true && docker rm $(docker ps -a -q) || true && docker rmi -f $(docker images -q) 
+|| true
+
+docker build -t backend .
+docker tag backend 19950818/k8s:backend 
+docker push 19950818/k8s:backend
