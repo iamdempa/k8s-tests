549508ecd786cd1c6966c9cfedddb829c396fa47
iamdempa <banukajananathjayarathna@gmail.com>
2021-05-15 20:21:50 +0530

    changed push

--- mern/backend/push.sh
+++ mern/backend/push.sh
@@ -1,5 +1,8 @@
-docker kill $(docker ps -q) || true && docker rm $(docker ps -a -q) || true && docker rmi -f $(docker images -q) || true
+docker kill $(docker ps -q) || true && docker rm $(docker ps -a -q) 
+# && docker rmi -f $(docker images -q) || true
 
-docker build -t backend .
-docker tag backend 19950818/k8s:backend 
+docker rmi -f backend:latest || true && docker rmi -f 19950818/k8s:backend || true
+
+docker build -t backend:latest .
+docker tag backend:latest 19950818/k8s:backend 
 docker push 19950818/k8s:backend
--- mern/frontend/push.sh
+++ mern/frontend/push.sh
@@ -1,5 +1,8 @@
-docker kill $(docker ps -q) || true && docker rm $(docker ps -a -q) || true && docker rmi -f $(docker images -q)  || true
+docker kill $(docker ps -q) || true && docker rm $(docker ps -a -q) || true 
 
-docker build -t frontend .
-docker tag frontend 19950818/k8s:frontend 
+# && docker rmi -f $(docker images -q)  || true
+
+docker rmi -f frontend:latest || true && docker rmi -f 19950818/k8s:frontend || true
+docker build -t frontend:latest .
+docker tag frontend:latest 19950818/k8s:frontend 
 docker push 19950818/k8s:frontend
